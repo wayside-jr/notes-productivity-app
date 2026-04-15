dc605c0ce103b0831d059ff3413b651e3cdb5344
Jeremy Junior <jrjn71780@gmail.com>
2026-04-15 13:54:01 +0300

    added sign up route

--- app.py
+++ app.py
@@ -23,5 +23,28 @@ def home():
     return {"message": "note productivity app"}
 
 
+# sign up route
+@app.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return {"error": "Username and password required"}, 400
+
+    if User.query.filter_by(username=username).first():
+        return {"error": "Username already exists"}, 400
+
+    user = User(username=username)
+    user.set_password(password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return {"message": "User created successfully"}, 201
+
+
 if __name__ == "__main__":
     app.run(debug=True)
